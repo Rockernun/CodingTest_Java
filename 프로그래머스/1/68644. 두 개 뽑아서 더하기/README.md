8d84113397c9f273f27df7445c2f4ebfd4f1b8ff
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-03-24 17:31:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 11.52 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 68.1 MB, 시간: 4.82 ms
+메모리: 71 MB, 시간: 11.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 17:25:04
+2025년 03월 24일 17:31:28
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,18 +1,16 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(int[] numbers) {
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i = 0; i < numbers.length - 1; i++) {
-            for(int j = i + 1; j < numbers.length; j++) {
-                list.add(numbers[i] + numbers[j]);
+     public int[] solution(int[] numbers) {
+        Set<Integer> set = new TreeSet<>();
+
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        Integer[] answer = list.stream().distinct().toArray(Integer[]::new);
-        Arrays.sort(answer);
-        
-        return Arrays.stream(answer).mapToInt(Integer::intValue).toArray();
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
