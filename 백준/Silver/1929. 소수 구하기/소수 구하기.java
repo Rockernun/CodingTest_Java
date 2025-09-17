9b32f5c96f8ccafe503088534bacf0ff4a1616bf
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-09-17 09:41:47 +0900

    [Silver III] Title: 소수 구하기, Time: 904 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,22 +1,37 @@
 import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = Math.max(2, m); i <= n; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j * j <= i; j++) {   
-                if (i % j == 0) {             
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                System.out.println(i);    
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        scanner.close();
+
+        List<Integer> primeList = new ArrayList<>();
+
+        // 입력된 값들 사이의 소수를 뽑아내기
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                primeList.add(i);
             }
         }
+
+        for (Integer integer : primeList) {
+            System.out.println(integer);
+        }
+
+    }
+
+    // 소수 판별 메서드
+    private static boolean isPrime(int value) {
+        if (value < 2) return false;
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
