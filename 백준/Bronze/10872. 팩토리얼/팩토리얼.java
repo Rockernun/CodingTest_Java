e7363bad5897ec50cc2a9e6edda77f200559ab98
Rockernun <114344042+Rockernun@users.noreply.github.com>
2025-12-16 14:22:03 +0900

    [Bronze III] Title: 팩토리얼, Time: 184 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(factorial(input));
+    }
+}
