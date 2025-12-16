05e41b29aa26d58427069f48d6313be7cc9e6293
Rockernun <114344042+Rockernun@users.noreply.github.com>
2025-12-16 13:15:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 276 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,26 +1,88 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int k = scanner.nextInt();
 
-        Deque<Integer> queue = new ArrayDeque<>();
+        List<Integer> result = new ArrayList<>();
+
         for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+            list.addLast(i);
         }
 
-        System.out.print("<");
-        while (queue.size() > 1) {
-            for (int i = 0; i < k - 1; i++) {
-                Integer polled = queue.poll();
-                queue.offer(polled);
-            }
-            System.out.print(queue.poll() + ", ");
+        int nextIndex = 0;
+        while (list.size != 0) {
+            nextIndex = (nextIndex + (k - 1)) % list.size;
+            result.add(list.remove(nextIndex));
         }
-        System.out.print(queue.poll() + ">");
+
+        String finalResult = result.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "<", ">"));
+        System.out.println(finalResult);
+    }
+
+    static class Node {
+        int element;
+        Node next;
+
+        public Node(int element) {
+            this.element = element;
+        }
+    }
+
+    static class MyLinkedList {
+        Node head;
+        int size = 0;
+
+        Node getLastNode() {
+            Node x = head;
+            while (x.next != null) {
+                x = x.next;
+            }
+
+            return x;
+        }
+
+        Node getNode(int index) {
+            Node x = head;
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+
+            return x;
+        }
+
+
+        void addLast(int element) {
+            Node newNode = new Node(element);
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node prevNode = getLastNode();
+                prevNode.next = newNode;
+            }
+
+            this.size++;
+        }
+
+        int remove(int index) {
+            Node removeNode = getNode(index);
+            if (index == 0) {
+                head = removeNode.next;
+            } else {
+                Node prevNode = getNode(index - 1);
+                prevNode.next = removeNode.next;
+            }
+
+            this.size--;
+            return removeNode.element;
+        }
     }
 }
