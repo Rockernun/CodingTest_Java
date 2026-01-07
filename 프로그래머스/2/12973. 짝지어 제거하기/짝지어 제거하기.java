33bcc7ae955b7ca6270e7889af2808f7f771aa04
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-01-07 23:08:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 65.42 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek().equals(s.charAt(i))) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
