23149bf3c0a9abd9e556f7c89016a3f1f0fe54a7
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-01-10 17:10:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.01 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public static String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : completion) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : participant) {
+            if (map.getOrDefault(s, 0) == 0) {
+                return s;
+            }
+            
+            map.put(s, map.getOrDefault(s, 0) - 1);
+        }
+        
+        return null;
+    }
+}
