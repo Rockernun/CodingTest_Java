b08c9e801924884c61e4f5f9b30961194d5484ab
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-01-15 15:23:05 +0900

    [level 1] Title: 폰켓몬, Time: 6.98 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int count = nums.length / 2;
+        
+        if (map.keySet().size() >= count) {
+            return count;
+        } else {
+            return map.keySet().size();
+        }
+    }
+}
