73fdca813aaef22919fbf6229df099d3330fcc6f
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-01-29 15:14:59 +0900

    [level 2] Title: 피보나치 수, Time: 5.48 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    static final Integer DIVISOR = 1234567;
+    
+    public int solution(int n) {
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            memo[i] = (memo[i - 1] + memo[i - 2]) % DIVISOR;
+        }
+        
+        return memo[n];
+    }
+}
