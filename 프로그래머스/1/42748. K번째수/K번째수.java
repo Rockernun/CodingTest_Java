a54f84aba80ab84740045f03a5f05dfa1a6089d8
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-01-31 22:01:57 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int n = commands.length;
+        int[] answer = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int[] copiedArray = Arrays.copyOf(array, array.length);
+            List<Integer> list = new ArrayList<>();
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                list.add(copiedArray[j]);
+            }
+            
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+        }
+        
+        return answer;
+    }
+}
