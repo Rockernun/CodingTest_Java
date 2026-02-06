48ce19ec38aa0ae8cf712ae778ecce59e27ab5c9
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-06 14:21:36 +0900

    [level 2] Title: 오픈채팅방, Time: 156.18 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -4,28 +4,38 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
+    private static final String ENTER = "Enter";
+    private static final String LEAVE = "Leave";
+    private static final String CHANGE = "Change";
+    private static List<String> resultList = new ArrayList<>();
+    
     public static List<String> solution(String[] record) {
-        Map<String, String> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-
-        for (String s : record) {
-            String[] splitCmd = s.split(" ");
-            if (splitCmd[0].equals("Enter")) {
-                map.put(splitCmd[1], splitCmd[2]);
-            } else if (splitCmd[0].equals("Change")) {
-                map.replace(splitCmd[1], splitCmd[2]);
+        Map<String, String> userInfo = new HashMap<>();
+        
+        for (String command : record) {
+            String[] splitCommand = command.split(" ");
+            
+            if (splitCommand[0].equals(ENTER)) {
+                userInfo.put(splitCommand[1], splitCommand[2]);
+            }
+            
+            if (splitCommand[0].equals(CHANGE)) {
+                userInfo.put(splitCommand[1], splitCommand[2]);
             }
         }
-
-        for (String s : record) {
-            String[] splitCmd = s.split(" ");
-            if (splitCmd[0].equals("Enter")) {
-                list.add(map.get(splitCmd[1]) + "님이 들어왔습니다.");
-            } else if (splitCmd[0].equals("Leave")) {
-                list.add(map.get(splitCmd[1]) + "님이 나갔습니다.");
+        
+        for (String command : record) {
+            String[] commands = command.split(" ");
+            
+            if (commands[0].equals(ENTER)) {
+                resultList.add(userInfo.get(commands[1]) + "님이 들어왔습니다.");
+            }
+            
+            if (commands[0].equals(LEAVE)) {
+                resultList.add(userInfo.get(commands[1]) + "님이 나갔습니다.");
             }
         }
-
-        return list;
+        
+        return resultList;
     }
 }
