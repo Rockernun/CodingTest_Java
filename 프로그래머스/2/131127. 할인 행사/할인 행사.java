ba9c8f081ac50f1e0aa4648e250b5af9aa4d1694
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-06 11:44:38 +0900

    [level 2] Title: 할인 행사, Time: 98.96 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,25 +3,24 @@ import java.util.Map;
 
 class Solution {
     public static int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wantMap = new HashMap<>();
-
+        Map<String, Integer> wantInfo = new HashMap<>();
+        int answer = 0;
+        
         for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
+            wantInfo.put(want[i], number[i]);
         }
         
-        int count = 0;
-
-        for (int i = 0; i < discount.length - 9; i++) {
-            Map<String, Integer> discountMap = new HashMap<>();
-            for (int j = i; j < i + 10; j++) {
-                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> discountInfo = new HashMap<>();
+            for (int j = 0; j < 10; j++) {
+                discountInfo.put(discount[i + j], discountInfo.getOrDefault(discount[i + j], 0) + 1);
             }
             
-            if (discountMap.equals(wantMap)) {
-                count++;
+            if (wantInfo.equals(discountInfo)) {
+                answer++;
             }
         }
-
-        return count;
+        
+        return answer;
     }
 }
