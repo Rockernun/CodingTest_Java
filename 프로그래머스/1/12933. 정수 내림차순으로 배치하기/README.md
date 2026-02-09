dec769cfae3ce093deb0bb0fd0999928443f354f
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-09 10:54:25 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 4.79 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 12.13 ms
+메모리: 80.5 MB, 시간: 4.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 31일 21:43:52
+2026년 02월 09일 10:54:24
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,22 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 class Solution {
     public long solution(long n) {
-        String[] list = (n + "").split("");
         
-        List<Integer> filteredList = new ArrayList<>();
-        for (String s : list) {
-            filteredList.add(Integer.parseInt(s));
-        }
+        String result = Arrays.stream(String.valueOf(n).split("")).sorted(Comparator.reverseOrder()).collect(Collectors.joining());
         
-        Collections.sort(filteredList);
-        Collections.reverse(filteredList);
-        
-        String answer = "";
-        for (Integer number : filteredList) {
-            answer += number + "";
-        }
-        
-        return Long.parseLong(answer);
+        return Long.parseLong(result);
     }
 }
