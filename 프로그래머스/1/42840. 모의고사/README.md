e44528066faf75653a00aabeb96e46c034b1995f
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-15 16:11:17 +0900

    [level 1] Title: 모의고사, Time: 2.40 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 7.87 ms
+메모리: 89.1 MB, 시간: 2.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 13:56:27
+2026년 02월 15일 16:11:14
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,75 @@
+import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
+    
+    static class Student {
+        int number;
+        int[] pattern;
+        int answerCount;
+        
+        public Student(int number, int[] pattern) {
+            this.number = number;
+            this.pattern = pattern;
+        }
+        
+        public int getStudentNumber() {
+            return number;
+        }
+        
+        public int[] getPattern() {
+            return pattern;
+        }
+        
+        public int getPatternLength() {
+            return pattern.length;
+        }
+        
+        public int getAnswerCount() {
+            return answerCount;
+        }
+    }
+    
     public int[] solution(int[] answers) {
-        int[][] patterns = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
-        int[] score = new int[patterns.length]; 
+        Student student1 = new Student(1, new int[]{1, 2, 3, 4, 5});
+        Student student2 = new Student(2, new int[]{2, 1, 2, 3, 2, 4, 2, 5});
+        Student student3 = new Student(3, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5});
         
-        for(int i = 0; i < answers.length; i++) {
-            for(int j = 0; j < patterns.length; j++) {
-                int user_answer = patterns[j][i % patterns[j].length];
-                if(answers[i] == user_answer) {
-                    score[j]++;
-                }
+        for (int i = 0; i < answers.length; i++) {
+            if (student1.getPattern()[i % student1.getPatternLength()] == answers[i]) {
+                student1.answerCount++;
+            }
+            
+            if (student2.getPattern()[i % student2.getPatternLength()] == answers[i]) {
+                student2.answerCount++;
+            }
+            
+            if (student3.getPattern()[i % student3.getPatternLength()] == answers[i]) {
+                student3.answerCount++;
             }
         }
         
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        Student[] students = {student1, student2, student3};
+        List<Integer> result = new ArrayList<>();
+        int max = 0;
         
-        ArrayList<Integer> correctList = new ArrayList<>();
-        for (int i = 0; i < score.length; i++) {
-            if (score[i] == maxScore) {
-                correctList.add(i + 1);
+        for (Student student : students) {
+            if (student.answerCount > max) {
+                max = student.answerCount;
             }
         }
         
-        return correctList.stream().mapToInt(Integer::intValue).toArray();
+        for (Student student : students) {
+            if (max == student.answerCount) {
+                result.add(student.number);
+            }
+        }
+        
+        int[] winners = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            winners[i] = result.get(i);
+        }
+        
+        return winners;
     }
 }
