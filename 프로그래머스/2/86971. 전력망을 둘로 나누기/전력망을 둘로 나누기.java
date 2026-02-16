254dfacbb019c0a994bd625bcc3392e117e59c3b
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-16 16:58:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.15 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,48 +1,50 @@
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 class Solution {
-    static boolean[] visited;
-    static ArrayList<Integer>[] adjList;
-    static int count = 0;
-
+    
+    static List<Integer>[] wiresInfo;
+    static int answer;
+    
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        adjList = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-
+        wiresInfo = new ArrayList[n + 1];
+        answer = Integer.MAX_VALUE;
+        
         for (int i = 1; i <= n; i++) {
-            adjList[i] = new ArrayList<>();
+            wiresInfo[i] = new ArrayList<>();
         }
-
+        
         for (int i = 0; i < wires.length; i++) {
-            adjList[wires[i][0]].add(wires[i][1]);
-            adjList[wires[i][1]].add(wires[i][0]);
+            wiresInfo[wires[i][0]].add(wires[i][1]);
+            wiresInfo[wires[i][1]].add(wires[i][0]);
         }
 
         for (int[] wire : wires) {
-            adjList[wire[0]].remove(Integer.valueOf(wire[1]));
-            adjList[wire[1]].remove(Integer.valueOf(wire[0]));
-
-            dfs(1);
-            answer = Math.min(answer, Math.abs(count - (n - count)));
+            int wireCut1 = wire[0];
+            int wireCut2 = wire[1];
+            boolean[] visited = new boolean[n + 1];
             
-            count = 0;
-            Arrays.fill(visited, false);
-            adjList[wire[0]].add(wire[1]);
-            adjList[wire[1]].add(wire[0]);
+            int count = dfs(1, visited, wireCut1, wireCut2);
+            answer = Math.min(answer, Math.abs(count - (n - count)));
         }
-
+        
         return answer;
     }
-
-    private void dfs(int node) {
-        visited[node] = true;
-        count++;
-        for (int nextNode : adjList[node]) {
-            if (!visited[nextNode]) {
-                dfs(nextNode);
+    
+    private int dfs(int start, boolean[] visited, int wireCut1, int wireCut2) {
+        visited[start] = true;
+        int count = 1;
+        
+        for (int nextWire : wiresInfo[start]) {
+            if (start == wireCut1 && nextWire == wireCut2 || nextWire == wireCut1 && start == wireCut2) {
+                continue;
+            }
+            
+            if (!visited[nextWire]) {
+                count += dfs(nextWire, visited, wireCut1, wireCut2);
             }
         }
+        
+        return count;
     }
 }
