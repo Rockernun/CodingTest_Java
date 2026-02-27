69ff7b7d5b2e5010a6becb50eccb27f75482a269
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-02-27 14:01:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.95 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,35 @@
 import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 
 class Solution {
-    private static final int[] dx = {-1, 1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1};
-
+    
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    
     public int solution(int[][] maps) {
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        Deque<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1});
+        visited = new boolean[maps.length][maps[0].length];
+        Deque<int[]> priorityQueue = new ArrayDeque<>();
+        priorityQueue.offer(new int[]{0, 0, 1});
         visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            if (current[0] == maps.length - 1 && current[1] == maps[0].length - 1) {
-                return current[2];
+        
+        while (!priorityQueue.isEmpty()) {
+            int[] currentPosition = priorityQueue.poll();
+            if (currentPosition[0] == maps.length - 1 && currentPosition[1] == maps[0].length - 1) {
+                return currentPosition[2];
             }
-
+            
             for (int i = 0; i < 4; i++) {
-                int nX = current[0] + dx[i];
-                int nY = current[1] + dy[i];
-
-                if (nX >= 0 && nY >= 0 && nX < maps.length && nY < maps[0].length && maps[nX][nY] == 1 && !visited[nX][nY]) {
+                int nX = currentPosition[0] + dx[i];
+                int nY = currentPosition[1] + dy[i];
+                
+                if (nX >= 0 && nX < maps.length && nY >= 0 && nY < maps[0].length && maps[nX][nY] == 1 && !visited[nX][nY]) {
                     visited[nX][nY] = true;
-                    queue.add(new int[]{nX, nY, current[2] + 1});
+                    priorityQueue.offer(new int[]{nX, nY, currentPosition[2] + 1});
                 }
             }
         }
-
+        
         return -1;
     }
 }
