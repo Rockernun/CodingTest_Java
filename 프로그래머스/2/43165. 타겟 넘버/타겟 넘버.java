c13c05615378a35e909d2d84d14b906c3233a4ee
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-05-20 10:19:15 +0900

    [level 2] Title: 타겟 넘버, Time: 3.32 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,20 +3,19 @@ class Solution {
     static int count = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        dfs(0, numbers, target, 0);
         return count;
     }
     
-    private void dfs(int[] numbers, int target, int sum, int depth) {
+    private void dfs(int depth, int[] numbers, int target, int sum) {        
         if (depth == numbers.length) {
             if (sum == target) {
                 count++;
             }
-            
             return;
         }
         
-        dfs(numbers, target, sum + numbers[depth], depth + 1);
-        dfs(numbers, target, sum - numbers[depth], depth + 1);
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);
     }
 }
