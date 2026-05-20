5ffa26574e770311bf784604b098b43e6ecfc21e
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-05-20 11:41:56 +0900

    [level 2] Title: 피로도, Time: 15.93 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,37 +1,70 @@
+import java.util.*;
+
 class Solution {
     
-    private int answer = 0;
-    
     public int solution(int k, int[][] dungeons) {
-        calculateDungeons(dungeons, 0, k);
+        int count = 0;
+
+        int[] indexes = new int[dungeons.length];
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            indexes[i] = i;
+        }
+        
+        List<List<Integer>> output = permutation(indexes, dungeons.length);
+        
+        for (int i = 0; i < output.size(); i++) {
+            int currentK = k;
+            int c = 0;
+
+            for (int j = 0; j < output.get(i).size(); j++) {
+                int index = output.get(i).get(j);
+
+                if (currentK < dungeons[index][0]) {
+                    break;
+                }
+                
+                currentK -= dungeons[index][1];
+                c++;
+            }
+            
+            count = Math.max(count, c);
+        }
+        
+        return count;
+    }
+    
+    private List<List<Integer>> permutation(int[] indexes, int r) {
+        List<List<Integer>> answer = new ArrayList<>();
+        boolean[] visited = new boolean[indexes.length];
+
+        backTrack(new ArrayList<>(), r, indexes, visited, answer);
+
         return answer;
     }
     
-    private void calculateDungeons(int[][] arr, int depth, int k) {
-        int count = 0;
-        if (depth == arr.length) {
-            for (int[] dungeonInfo : arr) {
-                if (k >= dungeonInfo[0]) {
-                    k -= dungeonInfo[1];
-                    count++;
-                } else {
-                    break;
-                }
-            }
-            
-            answer = Math.max(answer, count);
+    private void backTrack(
+        List<Integer> current,
+        int r,
+        int[] indexes,
+        boolean[] visited,
+        List<List<Integer>> answer
+    ) {
+        if (current.size() == r) {
+            answer.add(new ArrayList<>(current));
+            return;
         }
         
-        for (int i = depth; i < arr.length; i++) {
-            swap(arr, depth, i);
-            calculateDungeons(arr, depth + 1, k);
-            swap(arr, depth, i); 
+        for (int i = 0; i < indexes.length; i++) {
+            if (!visited[i]) {
+                current.add(indexes[i]);
+                visited[i] = true;
+
+                backTrack(current, r, indexes, visited, answer);
+
+                current.remove(current.size() - 1);
+                visited[i] = false;
+            }
         }
-    }
-    
-    private void swap(int[][] arr, int i, int j) {
-        int[] temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
