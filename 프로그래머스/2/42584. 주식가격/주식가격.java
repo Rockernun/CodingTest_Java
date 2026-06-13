04c45c003b9e0bdb69c5eeb7ba012ba6d564d4b5
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-06-14 00:20:03 +0900

    [level 2] Title: 주식가격, Time: 10.14 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,19 +1,19 @@
+// 각 시점에서의 가격 기준 떨어지지 않는 기간 계산
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-            
-        for (int i = 0; i < prices.length; i++) {
-            int second = 0;
+        
+        
+        for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                if (prices[i] <= prices[j]) {
-                    second++;
-                } else {
-                    second++;
+                if (prices[i] > prices[j]) {
+                    answer[i]++;
                     break;
                 }
+                answer[i]++;
             }
-            answer[i] = second;
         }
+        
         return answer;
     }
 }
