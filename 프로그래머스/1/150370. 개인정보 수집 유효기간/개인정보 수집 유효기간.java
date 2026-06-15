67a0037b8a8e759af5900af296d82c3135b6926c
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-06-15 17:15:30 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.57 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,4 +1,4 @@
- import java.util.*;
+import java.util.*;
 
 class Solution {
     
@@ -6,62 +6,45 @@ class Solution {
     
     public int[] solution(String today, String[] terms, String[] privacies) {
         for (String s : terms) {
-            String term = s.split(" ")[0];
-            int validityPeriod = Integer.parseInt(s.split(" ")[1]);
+            String[] split = s.split(" ");
+            String term = split[0];
+            int validityPeriod = Integer.parseInt(split[1]);
             termsValidityPeriod.put(term, validityPeriod);
         }
         
-        List<Integer> termsList = new ArrayList<>();
-        boolean[] result = calculateExpireDate(today, privacies);
-        int count = 0;
+        List<Integer> result = new ArrayList<>();
+        int todayDays = convertToDays(today);
         
-        for (int i = 0; i < result.length; i++) {
-            if (result[i]) {
-                termsList.add(i + 1);
-                count++;
+        for (int i = 0; i < privacies.length; i++) {
+            String[] split = privacies[i].split(" ");
+            
+            String startDate = split[0];
+            String term = split[1];
+            
+            int startDays = convertToDays(startDate);
+            int validityPeriod = termsValidityPeriod.get(term);
+            
+            int expireDays = startDays + validityPeriod * 28;
+            
+            if (todayDays >= expireDays) {
+                result.add(i + 1);
             }
         }
         
-        int[] answer = new int[count];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = termsList.get(i);
+        int[] answer = new int[result.size()];
+        
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
         
         return answer;
     }
     
-    private boolean[] calculateExpireDate(String today, String[] privacies) {
-        boolean[] haveToExpire = new boolean[privacies.length];
+    private int convertToDays(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
         
-        for (int i = 0; i < privacies.length; i++) {
-            String startDate = privacies[i].substring(0, 11);  // 동의 날짜
-            String term = privacies[i].substring(11);  // 약관
-            
-            int validityPeriod = termsValidityPeriod.get(term);  // 유효기간
-            int year = validityPeriod / 12;
-            int month = validityPeriod % 12;
-            
-            int expireYear = Integer.parseInt(startDate.substring(0, 4)) + year;
-            int expireMonth = Integer.parseInt(startDate.substring(5, 7)) + month;
-            int expireDay = Integer.parseInt(startDate.substring(8, 10));
-            
-            if (expireMonth > 12) {
-                expireYear++;
-                expireMonth = expireMonth % 12; 
-            }
-            
-            int todayYear = Integer.parseInt(today.substring(0, 4));
-            int todayMonth = Integer.parseInt(today.substring(5, 7));
-            int todayDay = Integer.parseInt(today.substring(8, 10));
-            
-            if (todayYear > expireYear) {
-                haveToExpire[i] = true;
-            } else if (todayYear == expireYear && todayMonth > expireMonth) {
-                haveToExpire[i] = true;
-            } else if (todayYear == expireYear && todayMonth == expireMonth && todayDay >= expireDay) {
-                haveToExpire[i] = true;
-            }
-        }
-        return haveToExpire;
+        return year * 12 * 28 + month * 28 + day;
     }
 }
