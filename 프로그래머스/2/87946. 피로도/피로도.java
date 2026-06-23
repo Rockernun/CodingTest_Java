add6268bbdd559b83c607f76f3b89af9ba6f9b20
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-06-23 15:51:48 +0900

    [level 2] Title: 피로도, Time: 2.30 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,68 +1,22 @@
-import java.util.*;
-
 class Solution {
     
-    public int solution(int k, int[][] dungeons) {
-        int count = 0;
-
-        int[] indexes = new int[dungeons.length];
-        
-        for (int i = 0; i < dungeons.length; i++) {
-            indexes[i] = i;
-        }
-        
-        List<List<Integer>> output = permutation(indexes, dungeons.length);
-        
-        for (int i = 0; i < output.size(); i++) {
-            int currentK = k;
-            int c = 0;
-
-            for (int j = 0; j < output.get(i).size(); j++) {
-                int index = output.get(i).get(j);
-
-                if (currentK < dungeons[index][0]) {
-                    break;
-                }
-                
-                currentK -= dungeons[index][1];
-                c++;
-            }
-            
-            count = Math.max(count, c);
-        }
-        
-        return count;
-    }
+    int answer = 0;
     
-    private List<List<Integer>> permutation(int[] indexes, int r) {
-        List<List<Integer>> answer = new ArrayList<>();
-        boolean[] visited = new boolean[indexes.length];
-
-        backTrack(new ArrayList<>(), r, indexes, visited, answer);
-
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        backTrack(dungeons, k, visited, 0);
         return answer;
     }
     
-    private void backTrack(
-        List<Integer> current,
-        int r,
-        int[] indexes,
-        boolean[] visited,
-        List<List<Integer>> answer
-    ) {
-        if (current.size() == r) {
-            answer.add(new ArrayList<>(current));
-            return;
+    private void backTrack(int[][] dungeons, int fatigue, boolean[] visited, int depth) {
+        if (answer < depth) {
+            answer = depth;
         }
         
-        for (int i = 0; i < indexes.length; i++) {
-            if (!visited[i]) {
-                current.add(indexes[i]);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && (fatigue >= dungeons[i][0])) {
                 visited[i] = true;
-
-                backTrack(current, r, indexes, visited, answer);
-
-                current.remove(current.size() - 1);
+                backTrack(dungeons, fatigue - dungeons[i][1], visited, depth + 1);
                 visited[i] = false;
             }
         }
