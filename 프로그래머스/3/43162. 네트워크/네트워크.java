dcf3b30757f3602eeab452f2e98a3f46cbdf141b
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-07-23 00:25:17 +0900

    [level 3] Title: 네트워크, Time: 1.46 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,54 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+// 0번부터 n-1번까지 컴퓨터가 있음 (0번, 1번, 2번 컴퓨터가 존재)
+// 0번은 1번과 연결되어 있음
+// 1번은 0번과 연결되어 있음
+// 2번은 2번과 연결되어 있음
+
+// 방문 여부를 초기화할 필요는 없음
+// 0번부터 시작해서 n-1번까지의 노드를 기준으로 DFS를 수행하면 될 듯
+// 모든 DFS 탐색이 종료될 때 count를 1씩 증가시킴
+// 모든 노드를 방문했을 때의 count를 반환
+import java.util.*;
 
 class Solution {
-    static Deque<Integer> queue;
-    static boolean[] visited;
-
+    
+    Map<Integer, List<Integer>> adjList;
+    boolean[] visited;
+    int count = 0;
+    
     public int solution(int n, int[][] computers) {
-        int result = 0;
+        adjList = new HashMap<>();
         visited = new boolean[n];
+        
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i, computers, n);
-                result++;
+            adjList.put(i, new ArrayList<>());
+        }
+        
+        // 컴퓨터 연결 정보 저장
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 현재 컴퓨터 번호와 인덱스 번호가 같지 않고, computers[i][j] == 1인 경우
+                if (i != j && computers[i][j] == 1) {
+                    adjList.get(i).add(j);
+                }
             }
         }
-
-        return result;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        
+        return count;
     }
-
-    static void bfs(int number, int[][] computers, int n) {
-        queue = new ArrayDeque<>();
-        queue.offer(number);
-        visited[number] = true;
-
-        while (!queue.isEmpty()) {
-            Integer currentComputer = queue.poll();
-            for (int i = 0; i < computers.length; i++) {
-                if (!visited[i] && computers[currentComputer][i] == 1) {
-                    visited[i] = true;
-                    queue.offer(i);
-                }
+    
+    private void dfs(int current) {
+        visited[current] = true;
+        
+        for (int next : adjList.get(current)) {
+            if (!visited[next]) {
+                dfs(next);
             }
         }
     }
