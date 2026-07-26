1b2b215c4e73eefb4e6cdc5897c1969821c79de5
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-07-26 11:19:06 +0900

    [level 2] Title: 피로도, Time: 1.76 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,22 @@
 class Solution {
     
     int answer = 0;
+    boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        backTrack(dungeons, k, visited, 0);
+        visited = new boolean[dungeons.length];
+        backTrack(dungeons, k, 0);
         return answer;
     }
     
-    private void backTrack(int[][] dungeons, int fatigue, boolean[] visited, int depth) {
-        if (answer < depth) {
-            answer = depth;
-        }
+    private void backTrack(int[][] dungeons, int fatique, int count) {  // 현재 피로도(fatique)와 탐험한 던전의 수를 셀(count)
+        answer = Math.max(answer, count);
         
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && (fatigue >= dungeons[i][0])) {
+            int requiredFatique = dungeons[i][0];
+            if (!visited[i] && (requiredFatique <= fatique)) {
                 visited[i] = true;
-                backTrack(dungeons, fatigue - dungeons[i][1], visited, depth + 1);
+                backTrack(dungeons, fatique - dungeons[i][1], count + 1);
                 visited[i] = false;
             }
         }
