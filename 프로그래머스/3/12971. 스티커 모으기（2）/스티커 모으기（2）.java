b1643088e43f35728e22be3da032a92875d79a5e
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-09-08 19:56:15 +0900

    [level 3] Title: 스티커 모으기(2), Time: 3.41 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,13 +1,18 @@
 class Solution {
     
     public int solution(int sticker[]) {
+        // 첫 번째 원소를 포함하는 경우
         int[] dp1 = new int[sticker.length - 1];
+        
+        // 마지막 원소를 포함하는 경우
         int[] dp2 = new int[sticker.length];
         
+        // 스티커가 1장인 경우, 그 스티커의 숫자를 반환
         if (sticker.length == 1) {
             return sticker[0];
         } 
         
+        // 스티커가 2장인 경우, 둘 중 큰 스티커의 숫자를 반환
         if (sticker.length == 2) {
             return Math.max(sticker[0], sticker[1]);
         }
@@ -15,6 +20,7 @@ class Solution {
         // 첫 번째 원소를 포함하는 원형 스티커
         dp1[0] = sticker[0];
         dp1[1] = Math.max(sticker[0], sticker[1]);
+        
         for (int i = 2; i < sticker.length - 1; i++) {
             dp1[i] = Math.max(sticker[i] + dp1[i - 2], dp1[i - 1]);
         }
@@ -22,6 +28,7 @@ class Solution {
         // 마지막 원소를 포함하는 원형 스티커
         dp2[1] = sticker[1];
         dp2[2] = Math.max(sticker[1], sticker[2]);
+        
         for (int i = 3; i < sticker.length; i++) {
             dp2[i] = Math.max(sticker[i] + dp2[i - 2], dp2[i - 1]);
         }
