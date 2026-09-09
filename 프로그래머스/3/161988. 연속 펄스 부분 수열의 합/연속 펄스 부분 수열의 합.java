f2a8d9e6038ed2fc418a45c19cf2527b5ff8167b
Rockernun <114344042+Rockernun@users.noreply.github.com>
2026-09-09 09:51:29 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 18.19 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,32 +1,47 @@
 class Solution {
     public long solution(int[] sequence) {
-        int[] startWithPositive = new int[sequence.length];
-        int[] startWithNegative = new int[sequence.length];
+        int seqLength = sequence.length;
         
-        for (int i = 0; i < sequence.length; i++) {
+        // +1로 시작하는 펄스 수열과 -1로 시작하는 펄스 수열
+        int[] startWithPositive = new int[seqLength];
+        int[] startWithNegative = new int[seqLength];
+        
+        // 각 경우의 펄스 수열을 적용한 최종 수열
+        for (int i = 0; i < seqLength; i++) {
             if (i % 2 == 0) {
-                startWithPositive[i] = sequence[i];
-                startWithNegative[i] = -sequence[i];
-            } else {
                 startWithPositive[i] = -sequence[i];
                 startWithNegative[i] = sequence[i];
+            } else {
+                startWithPositive[i] = sequence[i];
+                startWithNegative[i] = -sequence[i];
             }
         }
         
-        long[] dp1 = new long[sequence.length];
-        long[] dp2 = new long[sequence.length];
+        long[] dp1 = new long[seqLength];
+        long[] dp2 = new long[seqLength];
+        
+        if (dp1.length == 1) {
+            return Math.max(startWithPositive[0], startWithNegative[0]);
+        }
+        
+        // [2, 3, -6, 1, 3, -1, 2, 4]
+        // startWithPositive = [2, -3, -6, -1, 3, 1, 2, -4]
+        // startWithNegative = [-2, 3, 6, 1, -3, 1, -2, 4]
+        
+        // dp1 = [2, -1, -6, -1, 3, 4, 6, 2]
+        // dp2 = [-2, 3, 9, 10, 7, 8, 6, 10]
+        
         dp1[0] = startWithPositive[0];
         dp2[0] = startWithNegative[0];
         
-        for (int i = 1; i < sequence.length; i++) {
-            dp1[i] = Math.max(startWithPositive[i] + dp1[i - 1], startWithPositive[i]);
-            dp2[i] = Math.max(startWithNegative[i] + dp2[i - 1], startWithNegative[i]);
-        }
+        long max1 = dp1[0];
+        long max2 = dp2[0];
         
-        long max1 = Long.MIN_VALUE;
-        long max2 = Long.MIN_VALUE;
-        
-        for (int i = 0; i < sequence.length; i++) {
+        // dp[i]: i번까지의 연속 수열의 합과 i번의 원소의 값 중 더 큰 값
+        for (int i = 1; i < dp1.length; i++) {
+            dp1[i] = Math.max(dp1[i - 1] + startWithPositive[i], startWithPositive[i]);
+            dp2[i] = Math.max(dp2[i - 1] + startWithNegative[i], startWithNegative[i]);
+            
             if (dp1[i] > max1) {
                 max1 = dp1[i];
             }
@@ -38,5 +53,4 @@ class Solution {
         
         return Math.max(max1, max2);
     }
-
 }
