8b36303a2c6fc48c2d30a41bdf03e3597de23391
박병욱 <114344042+Rockernun@users.noreply.github.com>
2026-09-22 10:01:15 +0900

    [level 3] Title: 양과 늑대, Time: 2.12 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,52 +1,65 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     
-    static List<List<Integer>> tree;
-    static int maxSheep = 0;
+    int[] info;
+    int[][] leftOrRight;
+    int answer = 0;
     
     public int solution(int[] info, int[][] edges) {
-        tree = new ArrayList<>();
+        this.info = info;
+        leftOrRight = new int[info.length][2];
         
         for (int i = 0; i < info.length; i++) {
-            tree.add(new ArrayList<>());
+            leftOrRight[i][0] = -1;
+            leftOrRight[i][1] = -1;
         }
         
         for (int[] edge : edges) {
-            tree.get(edge[0]).add(edge[1]);
+            int parent = edge[0];
+            int child = edge[1];
+            
+            if (leftOrRight[parent][0] == -1) {
+                leftOrRight[parent][0] = child;
+            } else {
+                leftOrRight[parent][1] = child;
+            }
         }
         
-        List<Integer> candidates = new ArrayList<>();
-        candidates.add(0);
+        List<Integer> next = new ArrayList<>();
+        addChildren(next, 0);
+        dfs(1, 0, next);
         
-        dfs(candidates, 0, 0, info);
-        return maxSheep;
+        return answer;
     }
     
-    private void dfs(List<Integer> candidates, int wolf, int sheep, int[] info) {
+    private void dfs(int sheep, int wolf, List<Integer> candidates) {
+        answer = Math.max(answer, sheep);
+        
         for (int i = 0; i < candidates.size(); i++) {
-            int current = candidates.get(i);
+            int node = candidates.get(i);
             
-            int wolfSum = wolf;
-            int sheepSum = sheep;
+            int newSheep = sheep + (info[node] == 0 ? 1 : 0);
+            int newWolf = wolf + (info[node] == 1 ? 1 : 0);
             
-            if (info[current] == 0) {
-                sheepSum++;
-            } else {
-                wolfSum++;
-            }
-            
-            if (wolfSum >= sheepSum) {
+            if (newSheep <= newWolf) {
                 continue;
             }
             
-            maxSheep = Math.max(maxSheep, sheepSum);
-            
             List<Integer> newCandidates = new ArrayList<>(candidates);
-            newCandidates.remove(i);
-            newCandidates.addAll(tree.get(current));
+            newCandidates.remove(Integer.valueOf(node));
+            addChildren(newCandidates, node);
             
-            dfs(newCandidates, wolfSum, sheepSum, info);
+            dfs(newSheep, newWolf, newCandidates);
+        }
+    }
+    
+    private void addChildren(List<Integer> next, int startNode) {
+        for (int child : leftOrRight[startNode]) {
+            if (child != -1) {
+                next.add(child);
+            }
         }
     }
 }
